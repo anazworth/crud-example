23fd29bebca0299ebeded68f4db6e5e89c9f40d5
anazworth <98629900+anazworth@users.noreply.github.com>
2023-12-06 14:57:50 -0500

    added UserControllerTest

--- src/test/java/com/example/cruddemo/UserControllerTest.java
+++ src/test/java/com/example/cruddemo/UserControllerTest.java
@@ -1,4 +1,54 @@
+package com.example.cruddemo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerTest {
-  
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Test
+    void getAllUsers() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/users"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    void testCreateUser() throws Exception {
+        User newUser = new User();
+        newUser.setFirstName("John");
+        newUser.setLastName("Doe");
+        newUser.setAge(30);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/user/new")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newUser)))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("John"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("Doe"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.age").value(30));
+    }
 }
